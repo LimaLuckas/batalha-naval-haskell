dd0d717614bfcc7bd10a9b326c31862c2574d9d1
LimaLuckas <lfreire05@gmail.com>
2024-04-22 21:14:18 -0300

    Adição de arquivos via upload

---
+++ mesa.hs
@@ -0,0 +1,47 @@
+import System.Random
+
+data TipoNavio = Submarino | Destruidor | Cruzador deriving (Show, Eq)
+
+data Navio = Navio { tipoNavio :: TipoNavio, posicoes :: Int } deriving (Show)
+
+
+type Matriz = [[Char]]
+
+
+criarMatriz :: Matriz
+criarMatriz = replicate 10 (replicate 10 'O')
+
+
+adicionarVetor :: Matriz -> Int -> Int -> [Int] -> Matriz
+adicionarVetor matriz i j vetor
+    | length vetor > 10 = error "O comprimento do vetor é maior que o tamanho da matriz."
+    | i < 0 || i >= 10 || j < 0 || j >= 10 = error "Posição inicial fora dos limites da matriz."
+    | i + length vetor > 10 = error "O vetor não cabe na matriz a partir da posição inicial."
+    | otherwise = adicionarVetor' matriz i j vetor
+    where
+        adicionarVetor' :: Matriz -> Int -> Int -> [Int] -> Matriz
+        adicionarVetor' [] _ _ _ = []
+        adicionarVetor' (linha:rest) indiceLinha indiceColuna vetor
+            | indiceLinha == 0 = adicionarVetorALinha linha indiceColuna vetor : adicionarVetor' rest (indiceLinha - 1) indiceColuna vetor
+            | otherwise = linha : adicionarVetor' rest (indiceLinha - 1) indiceColuna vetor
+
+        adicionarVetorALinha :: [Int] -> Int -> [Int] -> [Int]
+        adicionarVetorALinha [] _ _ = []
+        adicionarVetorALinha (x:xs) indiceColuna vetor
+            | indiceColuna == 0 = vetor ++ adicionarVetorALinha xs (indiceColuna - 1) vetor
+            | otherwise = x : adicionarVetorALinha xs (indiceColuna - 1) vetor
+
+-- Função para gerar um vetor aleatório de acordo com o número de posições de um navio
+gerarVetor :: Navio -> IO [Int]
+gerarPosicao navio = do
+    take (posicoes navio) . randomRs (0, 9) <$> newStdGen
+
+-- Função para gerar uma posição aleatória na matriz
+gerarPosicao :: IO (Int, Int)
+gerarPosicao = do
+    gen <- newStdGen
+    let (i, gen') = randomR (0, 9) gen
+        (j, _) = randomR (0, 9) gen'
+    return (i, j)
+
+
